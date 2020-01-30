d1b873689e39b4b32f115c7a674dc0185cace49b
German Voytekhovich <ueuknjg@gmail.com>
2020-01-30 22:09:30 +0300

    update aliases

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -2,7 +2,7 @@
 
 alias ...="cd ../.."
 alias ll="ls -laF"
-alias gs="git status"
+alias gs="git status --short"
 alias flex="clear && neofetch --cpu_display infobar --cpu_temp | lolcat"
-alias gdiff="git diff --staged"
+alias gdiff="git diff"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%C(yellow)%d%Creset' --abbrev-commit"
