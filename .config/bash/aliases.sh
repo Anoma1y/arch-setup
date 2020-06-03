14b34b247663a7ad518399c5ab415aa724c16ee7
German Voytekhovich <ueuknjg@gmail.com>
2020-06-03 14:06:26 +0300

    added docker uid export, termite collor and new alias

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -3,6 +3,7 @@
 alias ...="cd ../.."
 alias ll="ls -laF"
 alias gs="git status --short"
+alias gsf="git status"
 alias flex="clear && neofetch --cpu_display infobar --cpu_temp | lolcat"
 alias gdiff="git diff"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%C(yellow)%d%Creset' --abbrev-commit"
--- .config/bash/exports.sh
+++ .config/bash/exports.sh
@@ -4,4 +4,6 @@ PATH="$HOME/.node_modules/bin:$PATH"
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export JAVA_HOME=/usr/lib/jvm/default
 export npm_config_prefix=~/.node_modules
+export UID
