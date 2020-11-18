15b2d14fee4824e3aa24a02e7fedd11cefa1ca34
German Voytekhovich <ueuknjg@gmail.com>
2020-11-18 15:13:28 +0300

    add new alias, fix transparency in picom, add android (sdk) path

--- .config/bash/exports.sh
+++ .config/bash/exports.sh
@@ -7,3 +7,7 @@ export LANG=en_US.UTF-8
 export JAVA_HOME=/usr/lib/jvm/default
 export npm_config_prefix=~/.node_modules
 export UID
+export ANDROID_SDK_ROOT=~/Android/Sdk
+export ANDROID_AVD_HOME=~/.android/avd
+export ANDROID_SDK_HOME=~/Android/Sdk
+export ANDROID_HOME=~/Android/Sdk
