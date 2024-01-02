48f47d5e59980ee2ec91fdccf7fd99ef57844015
German Voytekhovich <ueuknjg@gmail.com>
2024-01-02 14:03:33 +0300

    update config, migrate alactitty config

--- .config/bash/exports.sh
+++ .config/bash/exports.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export GOPATH=~/go
+
 PATH="$HOME/.node_modules/bin:$PATH"
 PATH="$HOME/bin:$PATH"
 PATH="$HOME/build/wine:$PATH"
@@ -18,8 +20,12 @@ export ANDROID_SDK_HOME=~/Android/Sdk
 export ANDROID_HOME=~/Android/Sdk
 
 [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
-#source /usr/share/nvm/nvm.sh
-#source /usr/share/nvm/bash_completion
-#source /usr/share/nvm/install-nvm-exec
-#unset npm_config_prefix
+
+# source /usr/share/nvm/bash_completion
+# source /usr/share/nvm/install-nvm-exec
+
+unset npm_config_prefix
+source /usr/share/nvm/nvm.sh
+# nvm alias default 16.20.0
+# nvm use stable
 
