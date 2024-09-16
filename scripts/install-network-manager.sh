bddb12b036f109a513d2a04c5a9e39bce14a77c5
German Voytekhovich <ueuknjg@gmail.com>
2024-09-17 01:48:46 +0300

    fix

--- scripts/install-network-manager.sh
+++ scripts/install-network-manager.sh
@@ -4,4 +4,4 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/functions/functions.sh"
 
 print_message info "Installs network management software"
 pacman -S --noconfirm --needed --color=always networkmanager
-systemctl enable --now NetworkManager
+systemctl enable NetworkManager
